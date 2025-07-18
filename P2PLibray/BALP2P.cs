c8a1d4c329c860501bd5f5cf89325e4c38177bbb
Hritik <94539105+hritik080400@users.noreply.github.com>
2025-07-18 23:14:02 +0530

    diplay list

--- P2PLibray/BALP2P.cs
+++ P2PLibray/BALP2P.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using P2PHelper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,19 @@ namespace P2PLibray
 {
     public class BALP2P
     {
+
+        MSSQL objSql = new MSSQL();
+
+        public async Task<DataSet> DisplayItemList()
+        {
+            Dictionary<String, String> param = new Dictionary<String, String>();
+            param.Add("@Flag", "ListRegister");
+
+            DataSet ds = await objSql.ExecuteStoredProcedureReturnDS("RegsterUser", param);
+
+            return ds;
+
+        }
 
     }
 }
